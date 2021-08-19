903d5286335d6100873a8cd550f54b659e72b312
DELORME <skempo74150@gmail.com>
2021-08-19 17:04:36 +0200

    Ajout score
    petit bug au niveau de l'ajout du score a chauqe fois quon tue un ennemi on gange le score x 2

---
+++ Tabasse_Zombie_Simulator_2.0/Assets/Script/MANAGER.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class MANAGER : Singleton<MANAGER>
+{
+    float Score;
+    [SerializeField] TextMeshProUGUI ScoreTxt;
+
+    private void Start()
+    {
+        Score = 0f;
+        UpdateDisplayScore();
+    }
+
+    // permet d'ajouter du score 
+    public void AddScore(float ScoreToAdd)
+    {
+        Score += ScoreToAdd;
+        UpdateDisplayScore();
+    }
+
+    // Mets a jour l'affichage du score 
+    void UpdateDisplayScore()
+    {
+        ScoreTxt.text = Score.ToString();
+    }
+}
